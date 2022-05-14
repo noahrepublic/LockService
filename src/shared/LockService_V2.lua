e5fe8f00de934ad2004723e257ba439bf0c200ac
noahrepublic <noahbear08@hotmail.com>
2022-05-14 15:57:53 -0300

    Finished most of the service
    Added the following:
    
    - Safe threshold when they run out of keys
    - Sanity check on the safe threshold against exploiters.
    - Added all the client functions (hopefully)
    
    TODO:
    - Write a handler to test
    - Debug

--- src/shared/LockService_V2.lua
+++ src/shared/LockService_V2.lua
@@ -1,3 +1,9 @@
+--[[ TODO:
+    - Remove the adding of k2 at the end. It's not needed.
+
+
+]]
+
 -- Services --
 
 local Players = game:GetService("Players");
@@ -18,6 +24,13 @@ if RunService:IsServer() then
     };
     LockService.Locks = {};
     LockService.Salt = {};
+    LockService.AmountPerPlayer = 10;
+
+    if not script:FindFirstChild("KeysConnector") then
+        local connectorRemote = Instance.new("RemoteEvent");
+        connectorRemote.Name = "KeysConnector";
+        connectorRemote.Parent = script;
+    end
 
     -- Private Functions --
 
@@ -54,12 +67,6 @@ if RunService:IsServer() then
                 local hashedKey = rndKey * k1
                 hashedKey += k2
                 hashedKey -= k3
-                local chance = math.random(0, 1)
-                if chance == 1 then
-                    -- when client checks, check the last #k2 digits = k2
-                    hashedKey = tostring(hashedKey) .. tostring(k2) -- put the k2 at the end
-                    hashedKey = tonumber(hashedKey)
-                end
                 key[1] = hashedKey;
                 table.insert(keys, key);
             else
@@ -70,10 +77,37 @@ if RunService:IsServer() then
         return keys;
     end
 
+    local function assignKeys(player, keys)
+        LockService.Keys[player.UserId] = keys;
+        script:FindFirstChild("KeysConnector"):FireClient(player, LockService.Keys[player.UserId]);
+    end
+
+    local function initSalt(player)
+        local saltConnector = Instance.new("RemoteEvent");
+        saltConnector.Name = player.UserId;
+        saltConnector.Parent = script;
+        saltConnector:FireClient(player, LockService.Salt[player.UserId]);
+        local conn = saltConnector.OnServerEvent:Connect(function(player)
+            local event = script:FindFirstChild(player.UserId);
+            if event then
+                event:Destroy();
+            end
+        end);
+        conn:Disconnect();
+    end
+
     local function onConnect(player)
         local time = os.time();
         LockService.Salt[player.UserId] = {#Players:GetPlayers() + math.random(0, 10000), #Players:GetPlayers() - game.PlaceVersion, time / (math.random(time/2, time))};
+        initSalt(player);
         LockService.Keys[player.UserId] = {};
+        local keys = genKeys(player, LockService.AmountPerPlayer);
+        assignKeys(player, keys);
+    end
+
+    local function onDisconnect(player)
+        LockService.Keys[player.UserId] = {};
+        LockService.Salt[player.UserId] = {};
     end
 
     -- key checking --
@@ -81,8 +115,8 @@ if RunService:IsServer() then
     local function checkKey(player, key)
         -- hashed keys are sent
         for i, v in pairs(LockService.Keys[player.UserId]) do
-            if v[1] == key then
-                return v;
+            if v[2] == key then
+                return i;
             end
         end
         -- check if key is in the valid keys
@@ -91,11 +125,21 @@ if RunService:IsServer() then
         return false
     end
 
-    local function lockedEvent(player, key, params)
-        -- do i use rawKey or hashed? hashed is better?
+    local function lockedEvent(player, key, params, callbackFunction)
+        -- raw key is sent
+        if key == nil and #LockService[player.UserId] == 0 then
+            callbackFunction(params);
+            -- TODO: ASSIGN MORE KEYS
+            local newKeys = genKeys(player, LockService.AmountPerPlayer);
+            assignKeys(player, newKeys);
+        elseif key == nil and #LockService[player.UserId] > 0 then
+            player:Kick("You are not allowed to do that.");
+        end
         local keyIndex = checkKey(player, key);
         if keyIndex then
             -- remove from the valid keys
+            table.remove(LockService.Keys[player.UserId], keyIndex);
+            print("Key removed" .. #LockService.Keys[player.UserId]);
         else
             player:Kick("Invalid key");
         end
@@ -109,32 +153,61 @@ if RunService:IsServer() then
         lock.callbackFunction = callbackFunction;
         table.insert(self.Locks, lock); -- TODO: ADD NAMING FOR EASIER SORTING
         -- connect the event
-        event:Connect(lockedEvent);
+        event.OnServerEvent:Connect(function(player, key, params)
+            lockedEvent(player, key, params, callbackFunction);
+        end)
     end
 
     -- Connections --
 
     Players.PlayerAdded:Connect(onConnect);
+    Players.PlayerRemoving:Connect(onDisconnect);
+
 elseif RunService:IsClient() then
-    -- Client Functions --
+    local salt = nil;
+    local currentKeys = {};
+    -- Private Client Functions --
+
+    local function getSalt()
+        local saltConnector = script:FindFirstChild(Players.LocalPlayer.UserId);
+        if saltConnector then
+            local conn = saltConnector.OnClientEvent:Connect(function(s)
+                salt = s;
+            end)
+            conn:Disconnect();
+            saltConnector:FireServer();
+        end
+    end
+
+    pcall(getSalt);
+    repeat
+        task.wait();
+    until salt ~= nil;
+    print(salt)
 
     local function deHash(key, salt)
         local k1 = salt[1];
         local k2 = salt[2];
         local k3 = salt[3];
-        local digitsToCheck = tostring(k2)
-        local amount = tostring(key):len()
-        local digits = string.sub(tostring(key), amount - digitsToCheck:len() + 1, amount)
-        if tonumber(digits) == tonumber(digitsToCheck) then
-            key = tostring(key):sub(1, #tostring(key) - 2)
-        else
-            return false;
-        end
         key += k3;
         key -= k2
         key /= k1;
         return key;
     end
+
+    local function keyConnector(keys)
+        currentKeys = keys;
+    end
+    -- Class Functions --
+
+    function LockService:FireLock(event, params, key)
+        local deHashedKey = deHash(key, salt);
+        event:FireServer(deHashedKey, params);
+    end
+    
+    -- Connections --
+
+    script:WaitForChild("KeysConnector").OnClientEvent:Connect(keyConnector);
 end
 
 return LockService;
