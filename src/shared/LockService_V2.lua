0fd15c121fe90ba98008e3b0b19b147210c78a35
noahrepublic <noahbear08@hotmail.com>
2022-05-14 14:32:00 -0300

    Added hashing + dehashing

--- src/shared/LockService_V2.lua
+++ src/shared/LockService_V2.lua
@@ -21,6 +21,15 @@ if RunService:IsServer() then
 
     -- Private Functions --
 
+    local function getKeyIndex(tbl, key)
+        for i = 1, #tbl do
+            if tbl[i] == key then
+                return i;
+            end
+        end
+        return false;
+    end
+
     local function genKeys(player, amount)
         --[[
             layout of keys:
@@ -33,10 +42,32 @@ if RunService:IsServer() then
         local k1 = LockService.Salt[player.UserId][1];
         local k2 = LockService.Salt[player.UserId][2];
         local k3 = LockService.Salt[player.UserId][3]; 
-        
-        local key = {};
+    
         local usedKeys = {}; -- makes sure there is 0 collisions with the keys
-        local rndKey = math.random(100, 10000);
+        for i = 1, amount do
+            local key = {};
+            local rndKey = math.random(100, 10000);
+            if getKeyIndex(usedKeys, rndKey) == false then
+                table.insert(usedKeys, rndKey);
+                key[2] = rndKey;
+                -- hash it
+                local hashedKey = rndKey * k1
+                hashedKey += k2
+                hashedKey -= k3
+                local chance = math.random(0, 1)
+                if chance == 1 then
+                    -- when client checks, check the last #k2 digits = k2
+                    hashedKey = tostring(hashedKey) .. tostring(k2) -- put the k2 at the end
+                    hashedKey = tonumber(hashedKey)
+                end
+                key[1] = hashedKey;
+                table.insert(keys, key);
+            else
+                -- very rare to happen
+                continue
+            end 
+        end
+        return keys;
     end
 
     local function onConnect(player)
@@ -48,29 +79,25 @@ if RunService:IsServer() then
     -- key checking --
 
     local function checkKey(player, key)
-        -- dehash key
-
+        -- hashed keys are sent
+        for i, v in pairs(LockService.Keys[player.UserId]) do
+            if v[1] == key then
+                return v;
+            end
+        end
         -- check if key is in the valid keys
         -- if so remove, and return true
         -- else kick and return false
-        return true
-    end
-
-    local function getKeyIndex(player, key)
-        local keys = LockService.Keys[player.UserId];
-        for i = 1, #keys do
-            if keys[i] == key then
-                return i;
-            end
-        end
-        return false;
+        return false
     end
 
     local function lockedEvent(player, key, params)
-        local rawKey
-        if checkKey(player, key) then
+        -- do i use rawKey or hashed? hashed is better?
+        local keyIndex = checkKey(player, key);
+        if keyIndex then
             -- remove from the valid keys
-            local keyIndex = getKeyIndex(player, key);
+        else
+            player:Kick("Invalid key");
         end
     end
 
@@ -88,6 +115,26 @@ if RunService:IsServer() then
     -- Connections --
 
     Players.PlayerAdded:Connect(onConnect);
+elseif RunService:IsClient() then
+    -- Client Functions --
+
+    local function deHash(key, salt)
+        local k1 = salt[1];
+        local k2 = salt[2];
+        local k3 = salt[3];
+        local digitsToCheck = tostring(k2)
+        local amount = tostring(key):len()
+        local digits = string.sub(tostring(key), amount - digitsToCheck:len() + 1, amount)
+        if tonumber(digits) == tonumber(digitsToCheck) then
+            key = tostring(key):sub(1, #tostring(key) - 2)
+        else
+            return false;
+        end
+        key += k3;
+        key -= k2
+        key /= k1;
+        return key;
+    end
 end
 
 return LockService;
